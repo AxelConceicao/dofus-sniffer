b9893754eb56a271dd74720d92a083d70e57eb94
Axel Conceicao <bladingpark@gmail.com>
2020-11-13 13:37:32 +0100

    Protocol builder works for almost every packets
    fews packets are still unhandled (SetCharacterRestrictionsMessage, GameRolePlayShowActorMessage, HousePropertiesMessage)

--- protocolBuilder.py
+++ protocolBuilder.py
@@ -18,33 +18,60 @@ class ProtocolBuilder:
             eprint('Unable to load protocol')
             exit(1)
 
-    def getMessageUsingID(self, ID):
-        return next(item for item in self.protocol['messages'] if item["protocolID"] == ID)
+    def getObjectByID(self, ID):
+        for objectType in ['messages', 'types']:
+            obj = next((item for item in self.protocol[objectType] if item["protocolID"] == ID), None)
+            if obj : return obj
+        eprint('Unable to get object with ID "' + str(ID) + '"')
+        exit(1)
 
-    def getMessageUsingName(self, name):
-        return next(item for item in self.protocol['messages'] if item["name"] == name)
+    def getObjectByName(self, name):
+        for objectType in ['messages', 'types']:
+            obj = next((item for item in self.protocol[objectType] if item["name"] == name), None)
+            if obj : return obj
+        eprint('Unable to get object with name "' + str(name) + '"')
+        exit(1)
 
-    def deserializeField(self, msg, field):
-        # print(field['name'])
-        return self.methods[field['write_method']]()
+    def deserializeField(self, obj, field):
+        if 'is_vector' in field:
+            vector = []
+            for _ in range(0, self.methods[field['write_length_method']]()):
+                if 'type_namespace' in field:
+                    if 'prefixed_by_type_id' in field:
+                        vector.append(self.deserializeObject(self.getObjectByID(self.methods[field['write_type_id_method']]())))
+                    else:
+                        vector.append(self.deserializeObject(self.getObjectByName(field['type'])))
+                else:
+                    vector.append(self.methods[field['write_method']]())
+            return vector
+        else:
+            if 'type_namespace' in field:
+                if 'prefixed_by_type_id' in field:
+                    return self.deserializeObject(self.getObjectByID(self.methods[field['write_type_id_method']]()))
+                else:
+                    return self.deserializeObject(self.getObjectByName(field['type']))
+            else:
+                return self.methods[field['write_method']]()
 
-    def builder(self, msg):
+    def deserializeObject(self, obj):
         content = {}
-        if msg['super_serialize']:
-            content = self.builder(self.getMessageUsingName(msg['super']))
-        # print('- ' + msg['super'])
-        content[msg['name']] = {}
-        for pos in range(0, len(msg['fields'])):
-            field = next(item for item in msg['fields'] if item["position"] == pos)
-            content[msg['name']][field['name']] = self.deserializeField(msg, field)
+        if obj['super_serialize']:
+            content = self.deserializeObject(self.getObjectByName(obj['super']))
+        content[obj['name']] = {}
+        for pos in range(0, len(obj['fields'])):
+            field = next(item for item in obj['fields'] if item["position"] == pos)
+            content[obj['name']][field['name']] = self.deserializeField(obj, field)
         return content
 
     def build(self, ID, data):
         self.content = {}
         self.data = data
         self.setMethods()
-        self.content = self.builder(self.getMessageUsingID(ID))
-        print('--')
+        try:
+            self.content = self.deserializeObject(self.getObjectByID(ID))
+        except KeyError:
+            eprint('Unable to deserialize message "' + str(ID) + '"')
+            print(', '.join(str(c) for c in data.data))
         return self.content
 
     def setMethods(self):
@@ -54,4 +81,9 @@ class ProtocolBuilder:
             'writeInt': self.data.readInt,
             'writeDouble': self.data.readDouble,
             'writeUTF': self.data.readUTF,
+            'writeVarInt': self.data.readVarInt,
+            'writeVarShort': self.data.readVarShort,
+            'writeVarLong': self.data.readVarLong,
+            'writeBoolean': self.data.readBoolean,
+            'writeFloat': self.data.readFloat
         }
